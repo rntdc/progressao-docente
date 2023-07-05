acb3c67369eb0a2e28ac64950a4b924bbe472e3a
Renato <rntdek@gmail.com>
2023-07-05 16:18:52 -0300

    UPDATE datatable to portuguese

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,8 @@ import DataTable from 'datatables.net-dt';
 console.log("a");
 
 let table = new DataTable('#myTable', {
-    responsive: true
+    responsive: true,
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/pt-PT.json',
+    },
 });
